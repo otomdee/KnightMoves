36ba74f22561f5c0a1ba8a97b5b76b3db4208feb
otomdee <otomdaniel65@gmail.com>
2024-09-23 21:14:29 +0100

    complete knightMoves function

--- knightMoves.js
+++ knightMoves.js
@@ -17,6 +17,63 @@ board.forEach((head) => {
     populate(head);
 })
 
+//bfs to find shortest path
+function knightMoves(start, targetSpot) {
+    let head = board[(start[0] * 8) + start[1]];
+    let queue = [];
+    let node = head;
+    let visited = [];
+    prev = [];
+    //populate prev
+    for (let i = 0; i < 64; i++) {
+        prev.push(-1);
+    }
+
+    do {
+            //check if we've found our required Node
+            if ((targetSpot[0] === node.spot[0]) && (targetSpot[1] === node.spot[1])) {
+                break;
+            }
+            //pop node from queue
+            queue.splice(0,1);
+            //add node to visited
+            visited.push(node.spot);
+            //add node children to queue
+            let innerNode = node.next;
+            while(innerNode) {
+                if (visited.includes((board[(innerNode.spot[0] * 8) + innerNode.spot[1]]).spot)) {
+                    innerNode = innerNode.next;
+                    continue;
+                }
+                else {
+                    queue.push(board[(innerNode.spot[0] * 8) + innerNode.spot[1]]);
+                    //change prev[child] to current node when queueing children of said node;
+                    prev[(innerNode.spot[0] * 8) + innerNode.spot[1]] = (node.spot[0] * 8) + node.spot[1];
+                    //tests
+                    innerNode = innerNode.next;
+                }
+            }
+            //go to first item on queue
+            node = queue[0];
+    }
+    while(queue[0]);
+
+    //construct path;
+    let prevRev = [];
+    let pos = (node.spot[0] * 8) + node.spot[1];
+    do {
+        prevRev.push(board[pos].spot);
+        pos = prev[pos];
+    }
+    while(!(pos === (head.spot[0] * 8) + head.spot[1]));
+    prevRev.push(head.spot);
+    prevRev.reverse();
+    console.log(`You made it in ${prevRev.length - 1} moves! Here's your path: `);
+    prevRev.forEach((item) => {
+        console.log(`\n${item}`);
+    })
+    return (prevRev);
+}
 
 function listTail(head) {
     let node = head;
